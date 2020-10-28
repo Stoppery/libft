77459faba1712b7add6d1806bc904ba1b2f3b968
Максим Мироманов <stoppery@192.168.1.2>
2020-10-28 23:20:54 +0300

    master

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsherie <dsherie@student.42.fr>            +#+  +:+       +#+        */
+/*   By: stoppery <stoppery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 17:57:56 by dsherie           #+#    #+#             */
-/*   Updated: 2020/10/28 20:45:44 by dsherie          ###   ########.fr       */
+/*   Updated: 2020/10/28 22:56:26 by stoppery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,11 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr (const char *s, int c);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 #endif
