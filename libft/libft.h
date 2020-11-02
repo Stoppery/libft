326a289e584e627c5de9dd77c3cd65a761f50216
Stoppery <58211981+Stoppery@users.noreply.github.com>
2020-11-02 03:46:36 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -21,7 +21,7 @@ void		*ft_memset(void *b, int c, size_t len);
 void		*ft_memcpy(void *dest, const void *src, size_t count);
 void		*ft_memccpy(void *dest, const void *src, int ch, size_t count);
 void		*ft_memmove (void *destination, const void *source, size_t n);
-void		*memchr (const void *arr, int c, size_t n);
+void		*ft_memchr (const void *arr, int c, size_t n);
 int			ft_memcmp (const void *arr1, const void *arr2, size_t n);
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
