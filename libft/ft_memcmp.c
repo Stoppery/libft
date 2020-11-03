c5676b2fb13b9250d520af8f7cc0d19d28c63c00
Максим Мироманов <stoppery@192.168.1.4>
2020-11-03 19:51:51 +0300

    master

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -20,7 +20,7 @@ int	ft_memcmp(const void *arr1, const void *arr2, size_t n)
 
 	str1 = (unsigned char*)arr1;
 	str2 = (unsigned char*)arr2;
-	i = 0;
+	i = -1;
 	while (++i < n)
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
