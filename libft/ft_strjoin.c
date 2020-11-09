cce5092c2fdf13e29bd70d55a6af08a153bf419e
Deborah Sherie <dsherie@ox-t5.msk.21-school.ru>
2020-11-09 18:04:11 +0300

    master

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dsherie <dsherie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 19:27:45 by dsherie           #+#    #+#             */
-/*   Updated: 2020/10/30 20:17:02 by dsherie          ###   ########.fr       */
+/*   Updated: 2020/11/09 18:03:29 by dsherie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,18 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	size_s1;
-	size_t	size_s2;
+	char	*arr;
 	size_t	i;
 	size_t	j;
-	char	*result;
 
 	i = -1;
 	j = -1;
-	if (s1 && s2)
-	{
-		size_s1 = ft_strlen(s1);
-		size_s2 = ft_strlen(s2) + 1;
-		if (!(result = malloc(size_s1 + size_s2)))
-			return (NULL);
-		while (++i < size_s1)
-			result[i] = s1[i];
-		while (++j < size_s2)
-			result[i + j] = s2[j];
-		result[i + j] = '\0';
-		return (result);
-	}
-	return (NULL);
+	if (!(s1 && s2) || !(arr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	while (s1[++i])
+		arr[i] = s1[i];
+	while (s2[++j])
+		arr[i + j] = s2[j];
+	arr[i + j] = '\0';
+	return (arr);
 }
